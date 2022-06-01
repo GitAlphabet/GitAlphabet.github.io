4b1360d6903e67f337991a790758be9c9ea6bcb9
gitalphabet <alphabet_it@163.com>
2022-06-01 18:13:50 +0800

    ✨ feat(测试):cicd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 yarn docs:build
-cd ./docs/.vuepress/dist
-git init
-git add -A
-git commit -m'deploy'
+# cd ./docs/.vuepress/dist
+# git init
+# git add -A
+# git commit -m'deploy'
 # git push -f git@github.com:GitAlphabet/blog.git master:gh-page
 git push -f git@github.com:GitAlphabet/GitAlphabet.github.io.git master
