3272968fb2119a83b9d8e8feadc33316b20b5d3d
gitalphabet <alphabet_it@163.com>
2022-06-03 10:01:40 +0800

    ✨ feat(cicd): 优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   markdown: {
     lineNumbers: true, // 开启代码行号
   },
-  // plugins: [['vuepress-plugin-code-copy', true]],
+  plugins: [['vuepress-plugin-code-copy', true]],
   head: [
     [
       'link',
