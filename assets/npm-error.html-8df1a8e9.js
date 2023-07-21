46bf4355d179fa36dd7f46c65a8b29cb351605ed
GitAlphabet <GitAlphabet@users.noreply.github.com>
2023-07-21 02:05:31 +0000

    deploy: GitAlphabet/blog@9b9adf28c9ad042ebe1866ac3fd1d6361fd1e5f3

---
+++ assets/npm-error.html-8df1a8e9.js
@@ -0,0 +1,16 @@
+import{_ as n,p as e,q as a,a1 as s}from"./framework-453091bf.js";const i={},d=s(`<h2 id="npm-err" tabindex="-1"><a class="header-anchor" href="#npm-err" aria-hidden="true">#</a> npm ERR</h2><h3 id="报错信息" tabindex="-1"><a class="header-anchor" href="#报错信息" aria-hidden="true">#</a> 报错信息</h3><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>sh: umi: command not found
+npm ERR! code ELIFECYCLE
+npm ERR! syscall spawn
+npm ERR! file sh
+npm ERR! errno ENOENT
+npm ERR! ant-design-pro@5.0.0 build: <span class="token code-snippet code keyword">\`umi build\`</span>
+npm ERR! spawn ENOENT
+npm ERR! 
+npm ERR! Failed at the ant-design-pro@5.0.0 build script.
+npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="解决方案" tabindex="-1"><a class="header-anchor" href="#解决方案" aria-hidden="true">#</a> 解决方案</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">npm</span> cache clean <span class="token parameter variable">--force</span>
+<span class="token function">rm</span> <span class="token parameter variable">-rf</span> node_modules
+<span class="token function">rm</span> <span class="token parameter variable">-rf</span> package-lock.json
+<span class="token function">npm</span> <span class="token function">install</span>
+<span class="token function">npm</span> run build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),r=[d];function l(c,o){return e(),a("div",null,r)}const p=n(i,[["render",l],["__file","npm-error.html.vue"]]);export{p as default};
---
+++ assets/type-interface.html-ed6aecfd.js
@@ -0,0 +1,64 @@
+import{_ as n,p as s,q as a,a1 as e}from"./framework-453091bf.js";const t={},p=e(`<h2 id="type和interface" tabindex="-1"><a class="header-anchor" href="#type和interface" aria-hidden="true">#</a> type和interface</h2><h3 id="相同点" tabindex="-1"><a class="header-anchor" href="#相同点" aria-hidden="true">#</a> 相同点</h3><ul><li><strong>都可以描述一个对象或者函数</strong></li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token comment">// interface</span>
+<span class="token keyword">interface</span> <span class="token class-name">User</span> <span class="token punctuation">{</span>
+ <span class="token literal-property property">name</span><span class="token operator">:</span> string
+ <span class="token literal-property property">age</span><span class="token operator">:</span> number
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">SetUser</span> <span class="token punctuation">{</span>
+ <span class="token punctuation">(</span>name<span class="token operator">:</span> string<span class="token punctuation">,</span> <span class="token literal-property property">age</span><span class="token operator">:</span> number<span class="token punctuation">)</span><span class="token operator">:</span> <span class="token keyword">void</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">// type</span>
+type User <span class="token operator">=</span> <span class="token punctuation">{</span>
+ <span class="token literal-property property">name</span><span class="token operator">:</span> string
+ <span class="token literal-property property">age</span><span class="token operator">:</span> number
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+type SetUser <span class="token operator">=</span> <span class="token punctuation">(</span>name<span class="token operator">:</span> string<span class="token punctuation">,</span> <span class="token literal-property property">age</span><span class="token operator">:</span> number<span class="token punctuation">)</span><span class="token operator">:</span> <span class="token keyword">void</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><p><strong>都允许拓展</strong><code>interface</code> 和 <code>type</code> 都可以拓展，并且两者并不是相互独立的，也就是说 <code>interface</code> 可以 <code>extends type</code>, <code>type</code> 也可以 <code>extends interface</code> 。 虽然效果差不多，但是两者语法不同。</p><ul><li><strong>interface extends interface</strong>(extends)</li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">interface</span> <span class="token class-name">Name</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">name</span><span class="token operator">:</span> string<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">User</span> <span class="token keyword">extends</span> <span class="token class-name">Name</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">age</span><span class="token operator">:</span> number<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><strong>type extends type</strong> (&amp;)</li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code>type Name <span class="token operator">=</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">name</span><span class="token operator">:</span> string<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+type User <span class="token operator">=</span> Name <span class="token operator">&amp;</span> <span class="token punctuation">{</span> <span class="token literal-property property">age</span><span class="token operator">:</span> number <span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><strong>interface extends type</strong>(extends)</li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code>type Name <span class="token operator">=</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">name</span><span class="token operator">:</span> string<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">User</span> <span class="token keyword">extends</span> <span class="token class-name">Name</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">age</span><span class="token operator">:</span> number<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><strong>type extends interface</strong> (extends)</li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">interface</span> <span class="token class-name">Name</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">name</span><span class="token operator">:</span> string<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+type User <span class="token operator">=</span> Name <span class="token operator">&amp;</span> <span class="token punctuation">{</span> 
+  <span class="token literal-property property">age</span><span class="token operator">:</span> number<span class="token punctuation">;</span> 
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li></ul><h3 id="不同点" tabindex="-1"><a class="header-anchor" href="#不同点" aria-hidden="true">#</a> 不同点</h3><ul><li><code>type</code> 可以而 <code>interface</code> 不行。<strong>type 可以声明基本类型别名，联合类型，元组等类型</strong></li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token comment">// 基本类型别名</span>
+type Name <span class="token operator">=</span> string
+<span class="token comment">// 联合类型</span>
+<span class="token keyword">interface</span> <span class="token class-name">Dog</span> <span class="token punctuation">{</span>
+ <span class="token function">wong</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">Cat</span> <span class="token punctuation">{</span>
+ <span class="token function">miao</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+type Pet <span class="token operator">=</span> Dog <span class="token operator">|</span> Cat
+<span class="token comment">// 具体定义数组每个位置的类型</span>
+type PetList <span class="token operator">=</span> <span class="token punctuation">[</span>Dog<span class="token punctuation">,</span> Pet<span class="token punctuation">]</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li><code>interface</code> 可以而 <code>type</code> 不行。<strong>interface 能够声明合并</strong></li></ul><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">interface</span> <span class="token class-name">User</span> <span class="token punctuation">{</span>
+ <span class="token literal-property property">name</span><span class="token operator">:</span> string
+ <span class="token literal-property property">age</span><span class="token operator">:</span> number
+<span class="token punctuation">}</span>
+<span class="token keyword">interface</span> <span class="token class-name">User</span> <span class="token punctuation">{</span>
+ <span class="token literal-property property">sex</span><span class="token operator">:</span> string
+<span class="token punctuation">}</span>
+<span class="token comment">/*
+User 接口为 {
+ name: string
+ age: number
+ sex: string 
+}
+*/</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,10),i=[p];function l(o,c){return s(),a("div",null,i)}const d=n(t,[["render",l],["__file","type-interface.html.vue"]]);export{d as default};
